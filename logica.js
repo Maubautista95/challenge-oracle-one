25c9c23b1b9629709a5f08a4e0a4f3d6c4038510
mbautista95 <mbautistarubiano@gmail.com>
2023-05-30 16:20:51 -0500

    Se añade botón de copia (aún no funciona), las funciones para encriptar y desencriptar funcionan correctamente.

--- logica.js
+++ logica.js
@@ -1,9 +1,29 @@
 function encriptar() {
-    let encriptado = document.getElementById('texto-a-transformar').value;
-    document.getElementById('resultado').innerHTML = encriptado;
+
+    const texto = document.getElementById('texto-a-transformar').value;
+    const encriptado = texto.replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+    document.getElementById('resultado').textContent = encriptado;
 }
 
 
 function desencriptar() {
 
+    const textoDesencriptado = document.getElementById('texto-a-transformar').value;
+    const desencriptado = textoDesencriptado.replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+    document.getElementById('resultado').textContent = desencriptado;
+
 }
+
+function copiar() {
+    var textoCopiado = document.getElementById('resultado');
+
+}
+
