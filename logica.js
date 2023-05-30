dccef17e9372581e4da808489df4a63115cc06da
mbautista95 <mbautistarubiano@gmail.com>
2023-05-30 12:37:09 -0500

    Añadido archivo JavaScript, que captura correctamente el texto y lo imprime en el párrafo del recuadro de resultados

---
+++ logica.js
@@ -0,0 +1,9 @@
+function encriptar() {
+    let encriptado = document.getElementById('texto-a-transformar').value;
+    document.getElementById('resultado').innerHTML = encriptado;
+}
+
+
+function desencriptar() {
+
+}
